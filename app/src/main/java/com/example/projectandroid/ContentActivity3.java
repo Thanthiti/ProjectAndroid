debb9f6e4b7a7775dbf109c4c6a16595a0ce4f19
thanayyy <thanayyy5998@gmail.com>
2025-03-26 16:59:44 +0700

    Test

--- app/src/main/java/com/example/projectandroid/ContentActivity3.java
+++ app/src/main/java/com/example/projectandroid/ContentActivity3.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,16 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ContentActivity1 extends AppCompatActivity implements View.OnClickListener{
+public class ContentActivity3 extends AppCompatActivity implements View.OnClickListener {
     private TextView btnBack, btnNext;
-    private ProgressBar progressBar;
+    private ProgressBar progressBar3;
     private ScrollView scrollView;
-    private Button btnQuiz1;
+    private Button btnQuiz3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_content1);
+        setContentView(R.layout.activity_content3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -34,39 +33,43 @@ public class ContentActivity1 extends AppCompatActivity implements View.OnClickL
         // ผูก UI กับตัวแปร
         btnBack = findViewById(R.id.btnBack);
         btnNext = findViewById(R.id.btnNext);
-        progressBar = findViewById(R.id.progressBar);
+        progressBar3 = findViewById(R.id.progressBar3);
         scrollView = findViewById(R.id.scrollView);
-        btnQuiz1 = findViewById(R.id.btnQuiz1);
+        btnQuiz3 = findViewById(R.id.btnQuiz3);
 
         // ตั้งค่าคลิก listener ให้ activity นี้จัดการเอง
         btnBack.setOnClickListener(this);
         btnNext.setOnClickListener(this);
-        btnQuiz1.setOnClickListener(this);
+        btnQuiz3.setOnClickListener(this);
 
         scrollView.getViewTreeObserver().addOnScrollChangedListener(() -> {
             View view = scrollView.getChildAt(0);
             int scrollY = scrollView.getScrollY(); // ตำแหน่งที่ Scroll อยู่
             int maxScroll = view.getBottom() - scrollView.getHeight(); // ความยาวเลื่อนสูงสุด
 
-            if (maxScroll > 0) {
-                // แปลงค่า Scroll เป็นเปอร์เซ็นต์ของ 20%
-                int progress = (int) (((float) scrollY / maxScroll) * 20);
-                progressBar.setProgress(progress);
+            if (maxScroll > 40) {
+                // แปลงค่า Scroll เป็นเปอร์เซ็นต์ของ 60%
+                int progress = 40 + (int) (((float) scrollY / maxScroll) * 20);
+                if (scrollY >= maxScroll) {
+                    progress = 60;
+                }
+                progressBar3.setProgress(progress);
             }
         });
     }
+
     @Override
     public void onClick(View v) {
         if (v == btnBack) {
-            Intent intent = new Intent(ContentActivity1.this, MainActivity.class);
+            Intent intent = new Intent(ContentActivity3.this, ContentActivity2.class);
             startActivity(intent);
             finish(); // ปิดหน้านี้เพื่อไม่ให้ย้อนกลับมา
         } else if (v == btnNext) {
-            Intent intent = new Intent(ContentActivity1.this, ContentActivity2.class);
-            startActivity(intent);
-            finish();
+            //Intent intent = new Intent(ContentActivity3.this, ContentActivity4.class);
+            //startActivity(intent);
+            //finish();
         } else {
-            //Intent intent = new Intent(ContentActivity1.this, Quiz1.class);
+            //Intent intent = new Intent(ContentActivity1.this, Quiz3.class);
             //startActivity(intent);
             //finish();
         }
