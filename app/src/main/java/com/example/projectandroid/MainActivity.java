1f9a7df4fb582fd2bb2caccc2460a35bf4f9e6c4
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-03-26 16:42:58 +0700

    new one

--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.projectandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +17,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ProgressBar progressBar;
-    Button btnProgress;
+    //Button btnProgress;
+    LinearLayout menu_data , menu_edit;
+    ImageView logout ;
+    int exp = 0;
+    TextView textExp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,15 +33,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return insets;
         });
         progressBar = findViewById(R.id.home_progressbar);
-        btnProgress = findViewById(R.id.home_btnProgress);
-        btnProgress.setOnClickListener(this);
+        //btnProgress = findViewById(R.id.home_btnProgress);
+        //btnProgress.setOnClickListener(this);
+        textExp = findViewById(R.id.home_textExp);
+        menu_data = findViewById(R.id.home_menu_data);
+        menu_data.setOnClickListener(this);
+        menu_edit = findViewById(R.id.home_menu_edit);
+        menu_edit.setOnClickListener(this);
+        logout = findViewById(R.id.home_logout);
+        logout.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.home_btnProgress) {
-            progressBar.incrementProgressBy(10);
+        if (view.getId() == R.id.home_menu_data) Toast.makeText(this, "test" , Toast.LENGTH_SHORT).show();
+        else if (view.getId() == R.id.home_menu_edit) {
+            Intent i = new Intent(this , EditActivity.class);
+            startActivity(i);
+        } else if (view.getId() == R.id.home_logout) {
+            
         }
-    }
+        //if (view.getId() == R.id.home_btnProgress) {
+            //if (exp >= 0 && exp < 100) {
+               // progressBar.incrementProgressBy(20);
+               // exp += 20;
+               // textExp.setText("Your experience is : " + exp + " %");
+           // } else Toast.makeText(this , "MAX" , Toast.LENGTH_SHORT).show();
+       // }
+   }
 }
